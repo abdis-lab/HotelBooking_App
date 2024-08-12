519336ff968b1a2a01e8adcd0a780de6c68f12c5
Abdi Kadir <abdisalamfresh@gmail.com>
2024-08-12 01:37:29 -0700

    Completed the Hotels UI and now moving on to adding Rooms in the DB

--- src/main/java/com/abdisalam/hotelbooking/controller/CustomerController.java
+++ src/main/java/com/abdisalam/hotelbooking/controller/CustomerController.java
@@ -1,4 +1,33 @@
 package com.abdisalam.hotelbooking.controller;
 
+import com.abdisalam.hotelbooking.model.Customer;
+import com.abdisalam.hotelbooking.repository.CustomerRepository;
+import com.abdisalam.hotelbooking.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class CustomerController {
+
+
+    @Autowired
+    private CustomerService customerService;
+
+
+    @GetMapping
+    public String listCustomer(Model model){
+       List<Customer> customer = customerService.getAllCustomers();
+       model.addAttribute("customer", customer);
+
+       return "rooms";
+    }
+
+
+
+
+
 }
---
+++ src/main/resources/static/javaScript/hotels.js
@@ -0,0 +1,5 @@
+const viewRoom = document.getElementById("viewRoom");
+
+viewRoom.addEventListener("click", () => {
+    console.log("You have clicked Me YEEEEAAAAA");
+});
